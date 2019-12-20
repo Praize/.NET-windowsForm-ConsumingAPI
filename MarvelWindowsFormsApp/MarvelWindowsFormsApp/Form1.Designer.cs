61d7c8855a0b5e44b3c7d2d813e85268a7287e08
Praize <senemenyuswa07@gmail.com>
2019-12-20 20:17:18 +0200

    first commit

---
+++ MarvelWindowsFormsApp/MarvelWindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace MarvelWindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gvMarvel = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.gvMarvel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gvMarvel
+            // 
+            this.gvMarvel.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvMarvel.Location = new System.Drawing.Point(238, 48);
+            this.gvMarvel.Name = "gvMarvel";
+            this.gvMarvel.Size = new System.Drawing.Size(340, 305);
+            this.gvMarvel.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Showcard Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.button1.Location = new System.Drawing.Point(299, 364);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(217, 31);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Close ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::MarvelWindowsFormsApp.Properties.Resources.endgame11;
+            this.ClientSize = new System.Drawing.Size(806, 399);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.gvMarvel);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.gvMarvel)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView gvMarvel;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ MarvelWindowsFormsApp/MarvelWindowsFormsApp/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Marvel.models;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace MarvelWindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadMarvelData();
+        }
+        //Hosted web API REST Service base url
+        string Baseurl = "http://localhost:8005/Help";
+        // GET: StudentGroups
+        private async void LoadMarvelData()
+        {
+            List<Character> StudentInfo = new List<Character>();
+
+            using (var client = new System.Net.Http.HttpClient())
+            {
+                //Passing service base url
+                client.BaseAddress = new Uri(Baseurl);
+                client.DefaultRequestHeaders.Clear();
+                //Define request data format
+                HttpResponseMessage Res = await client.GetAsync("api/Values/GetMarvelCharacters");
+
+                //Checking the response is successful or not which is sent using HttpClient
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api
+                    var EmpResponse = Res.Content.ReadAsStringAsync().Result;
+
+                    //Deserializing the response recieved from web api and storing into the Emp
+                    StudentInfo = JsonConvert.DeserializeObject<List<Character>>(EmpResponse);
+
+                }
+            }
+            //returning the employee list to view
+            gvMarvel.DataSource = StudentInfo;
+            gvMarvel.Refresh();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
